00326428c59930ad38cab2a3b6232e5496c37381
Bisnar <acaronar7@gmail.com>
2023-12-01 12:19:53 +0800

    [Bisnar] Search function & pagination

--- admin/admin.php
+++ admin/admin.php
@@ -23,6 +23,56 @@ if(mysqli_num_rows($select) > 0){
         <link rel="shortcut icon" href="../img/slsu.png">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+
+        <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <link href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap.min.css" rel="stylesheet"/>
+            <script>
+                $(document).ready(function(){
+                    $("#myInput").on("keyup",function(){
+                        var value=$(this).val().toLowerCase();
+                        $("#myTable tr").filter(function(){
+                            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                        });
+                    });
+                });
+            </script>
+
+            <style>
+                .pagination {
+                    display: flex;
+                    justify-content: flex-end;
+                    align-items: center;
+                    list-style: none;
+                    padding: 0;
+                }
+
+                .pagination li {
+                    margin-right: 5px;
+                }
+
+                .pagination li a,
+                .pagination li.disabled {
+                    display: inline-block;
+                    padding: 6px 12px;
+                    text-decoration: none;
+                    color: #333;
+                    border: 1px solid #ccc;
+                    border-radius: 3px;
+                    background-color: #fff;
+                }
+
+                .pagination li a:hover {
+                    background-color: #f5f5f5;
+                }
+
+                .pagination li.active a {
+                    background-color: #007bff;
+                    color: #fff;
+                    font-weight: bold;
+                }
+
+            </style>
     </head>
     <body>
         <!--- START OF SIDEBAR--->
@@ -36,19 +86,9 @@ if(mysqli_num_rows($select) > 0){
                     <h2>Admin Dashboard</h2>
                 </div>
                 <div class="user--info">
-                    <script>
-                        $(document).ready(function(){
-                            $("#Input").on("keyup",function(){
-                                var value=$(this).val().toLowerCase();
-                                $("#Table tr").filter(function(){
-                                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-                                });
-                            });
-                        });
-                    </script>
                     <div class="search--box">
                         <i class="fas fasolid fa-search"></i>
-                        <input type="text" id="Input" placeholder="Search"/>
+                        <input type="text" id="myInput" placeholder="Search" class="form-control"/>
                     </div>
                     <div class="notification">
                         <div class="notif-icon" onclick="toggleNotifi()">
@@ -155,9 +195,28 @@ if(mysqli_num_rows($select) > 0){
                             <th>Salary</th>
                             <th>Action</th>
                         </thead>
-                        <tbody>
+                        <tbody id="myTable">
                             <?php
                                 require_once "../connection.php";
+
+                                if(isset($_GET['page_no']) && $_GET['page_no']!="") {
+                                    $page_no = $_GET['page_no'];
+                                }else{
+                                    $page_no = 1;
+                                }
+
+                                $total_records_per_page = 10;
+                                $offset = ($page_no-1) * $total_records_per_page;
+                                $previous_page = $page_no - 1;
+                                $next_page = $page_no + 1;
+                                $adjacents = "2";
+
+                                $result_count=mysqli_query($conn, "SELECT COUNT(*) as total_records FROM jobs");
+                                $total_records = mysqli_fetch_array($result_count);
+                                $total_records = $total_records['total_records'];
+                                $total_no_of_pages = ceil($total_records / $total_records_per_page);
+                                $second_last = $total_no_of_pages - 1;
+
                                 $sql = mysqli_query($conn, "SELECT * FROM jobs ORDER BY jobid");
                                 $count =1;
                                 $row = mysqli_num_rows($sql);
@@ -182,6 +241,68 @@ if(mysqli_num_rows($select) > 0){
                         </tfoot>
                     </table>
                 </div>
+                <ul class="pagination pull-right">
+                    <li class="pull-left btn btn-default disabled">Showing page <?php echo $page_no. " of ". $total_no_of_pages;?></li>
+                    <li <?php if($page_no <= 1) {echo "class='disabled'";}?>>
+                    <a <?php if($page_no > 1) {echo "href='?page_no=$previous_page'";}?>>Previous</a>
+                    </li>
+
+                <?php
+                    if($total_no_of_pages <=10) {
+                        for($counter = 1; $counter <-$total_no_of_pages;$counter++) {
+                            if($counter == $page_no) {
+                                echo "<li class='active'><a>$counter</a></li>";
+                            }else{
+                                echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                            }
+                        }
+                    }elseif($total_no_of_pages > 10) {
+                        if($page_no <=4){
+                            for($counter =  1; $counter < 8; $counter++){
+                                if($counter == $page_no){
+                                    echo "<li class='active'><a>$counter</a></li>";
+                                }else{
+                                    echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                                }
+                            }
+                            echo "<li><a>...</a></li>";
+                            echo "<li><a href='?page_no=$second_last'>$second_last</a></li>";
+                            echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                        }elseif($page_no > 4 && $page_no < $total_no_of_pages - 4){
+                            echo "<li><a href='>page_no=1'>1</a></li>";
+                            echo "<li><a href='>page_no=2'>2</a></li>";
+                            echo "<li><a>...</a></li>";
+
+                            for($counter = $page_no - $adjacents; $counter <=$page_no + $adjacents;$counter++){
+                                if($counter == $page_no){
+                                    echo "<li class='active'><a>$counter</a></li>";
+                                }else{
+                                    echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                                }
+                            }
+                            echo "<li><a>...</a></li>";
+                            echo "<li><a href='?page_no=$second_last'>$second_last</a></li>";
+                            echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                        }else{
+                            echo "<li><a href='>page_no=1'>1</a></li>";
+                            echo "<li><a href='>page_no=2'>2</a></li>";
+                            echo "<li><a>...</a></li>";
+
+                            for($counter = $total_no_of_pages - 6;$counter <= $total_no_of_pages;$counter++){
+                                if($counter == $page_no){
+                                    echo "<li class='active'><a>$counter</a></li>";
+                                }else{
+                                    echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                                }
+                            }
+                        }
+                    }
+                ?>
+                <li <?php if($page_no >= $total_no_of_pages){ echo "class='disabled'";}?>>
+                    <a <?php if($page_no < $total_no_of_pages){ echo "href='?page_no=$next_page'";}?>>Next</a>
+                </li>
+                <?php if($page_no < $total_no_of_pages){ echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo; &rsaquo;</></li>";}?>
+                </ul>
             </div>
         </div>
         <script src="../js/script.js"></script>
